caf6dd9b67ddc2456c3efa2c0bf98adfccc4cc19
chekalin <stanislav.chekalin@gmail.com>
2016-03-05 16:00:46 +0000

    cleanup

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const VideoDetail = ({video}) => {
     if (!video) {
@@ -10,7 +10,7 @@ const VideoDetail = ({video}) => {
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
-              <iframe className="embed-responsive-item" src={url}></iframe>
+                <iframe className="embed-responsive-item" src={url}></iframe>
             </div>
             <div className="details">
                 <div>{video.snippet.title}</div>
