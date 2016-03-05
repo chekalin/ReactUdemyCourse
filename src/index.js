d3c7b9bd73e13d65e8c51baf3199233fae52e0ed
chekalin <stanislav.chekalin@gmail.com>
2016-03-05 16:16:44 +0000

    extracted YouTube API key into separate file and added to ignore

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-
-const API_KEY = 'XXX';
-
+import API_KEY from "./resources/YOUTUBE_API_KEY"
 
 class App extends Component {
     constructor(props) {
